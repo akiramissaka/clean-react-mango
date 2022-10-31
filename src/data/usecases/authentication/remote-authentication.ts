8f2acc580d4d948926b8d1a6421809e20e48bcd6
Akira Missaka <akira.missaka@gmail.com>
2022-10-31 02:08:19 -0300

    chore: test commit husky

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from './protocols/http/http-post-client'
 
 export class RemoteAuthentication {
+    private readonly x: string = ''
     constructor (
         private readonly url: string,
         private readonly httpPostClient: HttpPostClient) {}
