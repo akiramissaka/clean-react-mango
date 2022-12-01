3e46d11bbb554d22fcb8efc2eea84b459e87c897
Akira Missaka <akira.missaka@gmail.com>
2022-12-01 02:50:27 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,24 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
 import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from './test/mock-http-client'
 
-console.log('dassdasd')
+type SutTypes = {
+    sut: RemoteAuthentication
+    httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        class HttpPostClientSpy implements HttpPostClient {
-            url?: string
-            async post (url: string): Promise<void> {
-                this.url = url
-            }
-        }
-        const httpPostClientSpy = new HttpPostClientSpy()
-        const url = 'any_url'
-        const sut = new RemoteAuthentication(url, httpPostClientSpy)
+        const url = 'other_url'
+        const { httpPostClientSpy, sut } = makeSut(url)
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
     })
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,10 +1,10 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteAuthentication {
-    private readonly xssss: string = ''
     constructor (
         private readonly url: string,
-        private readonly httpPostClient: HttpPostClient) {}
+        private readonly httpPostClient: HttpPostClient
+    ) {}
 
     async auth (): Promise<void> {
         await this.httpPostClient.post(this.url)
