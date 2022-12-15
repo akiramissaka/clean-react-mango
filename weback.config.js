a0005ef0ccdfb0da7c384913558669030bedee79
Akira Missaka <akira.missaka@gmail.com>
2022-12-15 01:36:44 -0300

    chore: config sass

--- weback.config.js
+++ weback.config.js
@@ -1,5 +1,5 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = {
     mode: 'development',
@@ -10,10 +10,36 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        option: {
+                            modules: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
     },
     devServe: {
         contentBase: './public',
